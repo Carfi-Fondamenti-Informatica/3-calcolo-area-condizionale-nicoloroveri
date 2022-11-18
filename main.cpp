6169f4387a68db34c945ab51df13ee024b6c0246
nicoloroveri <116441124+nicoloroveri@users.noreply.github.com>
2022-11-18 08:31:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
+   float a=0, b=0;
+   int opzione=0;
+   cin >> a >> b >> opzione;
+   switch(opzione){
+      case 0:
+         cout << ((a*b)/2) <<endl;//area triangolo
+         break;
+      case 1:
+         cout << (a*a) <<endl;//area quadrato
+         break;
+      case 2:
+         cout << (a*b) <<endl;//area rettangolo
+         break;
+      default:
+         cout << "opzione non valida" <<endl;
+   }
    return 0;
 }
